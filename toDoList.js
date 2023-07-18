6514b1c599558301a21062e191396f312190f64b
AmyGois <ahagois@hotmail.com>
2023-07-18 20:39:53 +0100

    Create constructors for tasks and projects

---
+++ toDoList.js
@@ -0,0 +1,30 @@
+/* ******************************************
+Constructors - Task, Step & Project
+  - Step goes inside Task.steps[]
+  - Task goes inside Project.tasks[]
+****************************************** */
+class Task {
+  constructor(title, description, dueDate, priority, status) {
+    this.title = title;
+    this.description = description;
+    this.steps = [];
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = status;
+  }
+}
+
+class Step {
+  constructor(description, status) {
+    this.title = description;
+    this.status = status;
+  }
+}
+
+class Project {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.tasks = [];
+  }
+}
