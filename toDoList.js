de37d167e1d0197e6d99c39e88caaf2665d349fe
AmyGois <ahagois@hotmail.com>
2023-07-19 19:17:47 +0100

    Create module to manipulate Project objects

--- toDoList.js
+++ toDoList.js
@@ -1,8 +1,17 @@
-/* ******************************************
+/* Contents:
+
+Constructors - Task, Step & Project
+
+Functions to deep clone arrays and objects
+
+Project Manager
+*/
+
+/* ********************************************************************
 Constructors - Task, Step & Project
   - Step goes inside Task.steps[]
   - Task goes inside Project.tasks[]
-****************************************** */
+******************************************************************** */
 class Task {
   constructor(title, description, dueDate, priority, status) {
     this.title = title;
@@ -28,3 +37,94 @@ class Project {
     this.tasks = [];
   }
 }
+
+/* ********************************************************************
+Functions to deep clone arrays and objects
+******************************************************************** */
+
+const deepCopyArray = (array) => {
+  const arrayCopy = [];
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      arrayCopy.push(deepCopyArray(item));
+    } else if (typeof item === 'object') {
+      arrayCopy.push(deepCopyObject(item));
+    } else {
+      arrayCopy.push(item);
+    }
+  });
+  return arrayCopy;
+};
+
+const deepCopyObject = (object) => {
+  const objectCopy = {};
+  for (const [key, value] of Object.entries(object)) {
+    if (Array.isArray(value)) {
+      objectCopy[key] = deepCopyArray(value);
+    } else if (typeof value === 'object') {
+      objectCopy[key] = deepCopyObject(value);
+    } else {
+      objectCopy[key] = value;
+    }
+  }
+  return objectCopy;
+};
+
+/* ********************************************************************
+Project Manager
+  - Create & delete projects
+  - Edit project titles & descriptions
+  - Create a safe copy of the projects array for public use
+******************************************************************** */
+const projects = [];
+
+const projectManager = (() => {
+  const createNewProject = (title, description) => {
+    const projectAlreadyExists = projects.find((project) => {
+      return project.title === title;
+    });
+
+    if (typeof projectAlreadyExists === 'object') {
+      return false; // Can't create new project because one with the same title already exists
+    }
+    const newProject = new Project(title, description);
+    projects.push(newProject);
+  };
+
+  const deleteProject = (projectIndex) => {
+    const index = Number(projectIndex);
+    projects.splice(index, 1);
+  };
+
+  const deleteProjectByName = (projectTitle) => {
+    projects.forEach((project) => {
+      if (project.title === projectTitle) {
+        projects.splice(projects.indexOf(project), 1);
+      }
+    });
+  };
+
+  const editProjectTitle = (projectIndex, newTitle) => {
+    projects[projectIndex].title = newTitle;
+  };
+
+  const editProjectDescription = (projectIndex, newDescription) => {
+    projects[projectIndex].description = newDescription;
+  };
+
+  const revealAllProjects = () => {
+    const projectsCopy = deepCopyArray(projects);
+    return projectsCopy;
+  };
+
+  return {
+    createNewProject,
+    deleteProject,
+    deleteProjectByName,
+    editProjectTitle,
+    editProjectDescription,
+    revealAllProjects,
+  };
+})();
+
+export { projectManager };
